aacd1f4de682e3321fbf4582ca3ce0e53a8d964f
Vineet Kalghatgi <vkalghat@gmail.com>
2024-05-12 19:55:18 -0400

    add utilitiy to load variables from .env file

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"inventory/utilities"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// func initDB() {
+// 	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// }
+
 func main() {
 
 	r := mux.NewRouter()
+
+	utilities.LoadEnv()
 
 	err := http.ListenAndServe("0.0.0.0:3000", r)
 
